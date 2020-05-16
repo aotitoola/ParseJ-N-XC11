2f5b128f72d05d1e532e95a22cc8685f2dd62069
Aanuoluwa Otitoola <aanuoluwa.otitoola@gmail.com>
2020-05-16 18:01:08 +0100

    Initial Commit

---
+++ ParseJ-N-XC11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ParseJ-N-XC11
+//
+//  Created by Aanuoluwa Otitoola on 16/05/2020.
+//  Copyright © 2020 Aanuoluwa Otitoola. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
